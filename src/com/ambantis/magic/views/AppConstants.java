1bb6f69ee39f90c62de0b6b9322eee7f9d7a660f
Rasmi Elasmar <rrelasmar@gmail.com>
2013-06-15 15:45:28 -0700

    Cleaning up/re-tooling UI

--- src/com/ambantis/magic/views/AppConstants.java
+++
@@ -1,9 +0,0 @@
-package com.ambantis.magic.views;
-
-public class AppConstants {
-	   public static final String TAB_A  = "tab_a_identifier";
-	   public static final String TAB_B  = "tab_b_identifier";
-
-	   //Insert other universal constants here.
-
-}
