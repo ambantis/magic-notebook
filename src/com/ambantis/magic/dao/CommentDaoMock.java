b85958641d9048a89c0562884caf1b006f480c6a
Alexandros Bantis <ambantis@gmail.com>
2013-06-16 10:41:21 -0700

    yet another stackoverflow fix

--- src/com/ambantis/magic/dao/CommentDaoMock.java
+++ src/com/ambantis/magic/dao/CommentDaoMock.java
@@ -27,12 +27,11 @@ public class CommentDaoMock implements CommentDao {
     @Override
     public ArrayList<Comment> readAll() throws DaoException, DaoConnectionException {
         ArrayList<Comment> tmpComments = new ArrayList<Comment>();
-        ArrayList<Student> tmpStudents = DaoFactory.getInstance().getStudentDao().readAll();
-        Comment c1 = new Comment("1", "1", new Date(), tmpStudents.get(4).getFirstName(), "Nice job!", "1");
-        Comment c2 = new Comment("2", "2", new Date(), tmpStudents.get(3).getFirstName(), "Nice job!", "1");
-        Comment c3 = new Comment("3", "3", new Date(), tmpStudents.get(2).getFirstName(), "Nice job!", "1");
-        Comment c4 = new Comment("4", "4", new Date(), tmpStudents.get(1).getFirstName(), "Nice job!", "1");
-        Comment c5 = new Comment("5", "5", new Date(), tmpStudents.get(0).getFirstName(), "Nice job!", "1");
+        Comment c1 = new Comment("1", "1", new Date(), "bill", "Nice job!", "1");
+        Comment c2 = new Comment("2", "2", new Date(), "james", "Nice job!", "1");
+        Comment c3 = new Comment("3", "3", new Date(), "nancy", "Nice job!", "1");
+        Comment c4 = new Comment("4", "4", new Date(), "cindy", "Nice job!", "1");
+        Comment c5 = new Comment("5", "5", new Date(), "roger", "Nice job!", "1");
         tmpComments.add(c1);
         tmpComments.add(c2);
         tmpComments.add(c3);
